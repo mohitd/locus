32cc59316a71c5d33b660bd7264a054dc597d114
Mohit Deshpande <mohitd2000@dhcp-128-146-147-52.osuwireless.ohio-state.edu>
2014-11-06 09:19:40 -0500

    Changes to the nav drawer so it's not excessively colorful.

--- app/src/main/java/com/centauri/locus/TaskListFragment.java
+++ app/src/main/java/com/centauri/locus/TaskListFragment.java
@@ -10,7 +10,10 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.ActionMode;
 import android.view.Menu;
@@ -59,7 +62,7 @@ public class TaskListFragment extends ListFragment implements AbsListView.MultiC
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Cursor cursor = null;
+        Cursor cursor;
         if (getArguments() != null && getArguments().getInt(MainActivity.KEY_TASK_ID) == 0) {
             Log.i(TAG, "Completed tasks");
             cursor = getActivity().getContentResolver().query(Locus.Task.CONTENT_URI, PROJECTION,
@@ -153,6 +156,7 @@ public class TaskListFragment extends ListFragment implements AbsListView.MultiC
         MenuInflater inflater = getActivity().getMenuInflater();
         inflater.inflate(R.menu.list_view_cab, menu);
         ids = new ArrayList<Long>();
+        this.actionMode = actionMode;
         return true;
     }
 
@@ -162,7 +166,11 @@ public class TaskListFragment extends ListFragment implements AbsListView.MultiC
      */
     @Override
     public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
-        return false;
+        final int checked = getListView().getCheckedItemCount();
+        CharSequence text = color(getResources().getColor(R.color.primary),
+                getResources().getString(R.string.cab_title, checked));
+        actionMode.setTitle(text);
+        return true;
     }
 
     /**
@@ -231,5 +239,59 @@ public class TaskListFragment extends ListFragment implements AbsListView.MultiC
 
         getListView().setItemChecked(position, true);
         return true;
+    }
+
+    /**
+     * Returns a CharSequence that concatenates the specified array of CharSequence
+     * objects and then applies a list of zero or more tags to the entire range.
+     *
+     * @param content an array of character sequences to apply a style to
+     * @param tags the styled span objects to apply to the content
+     *        such as android.text.style.StyleSpan
+     *
+     */
+    private static CharSequence apply(CharSequence[] content, Object... tags) {
+        SpannableStringBuilder text = new SpannableStringBuilder();
+        openTags(text, tags);
+        for (CharSequence item : content) {
+            text.append(item);
+        }
+        closeTags(text, tags);
+        return text;
+    }
+
+    /**
+     * Iterates over an array of tags and applies them to the beginning of the specified
+     * Spannable object so that future text appended to the text will have the styling
+     * applied to it. Do not call this method directly.
+     */
+    private static void openTags(Spannable text, Object[] tags) {
+        for (Object tag : tags) {
+            text.setSpan(tag, 0, 0, Spannable.SPAN_MARK_MARK);
+        }
+    }
+
+    /**
+     * "Closes" the specified tags on a Spannable by updating the spans to be
+     * endpoint-exclusive so that future text appended to the end will not take
+     * on the same styling. Do not call this method directly.
+     */
+    private static void closeTags(Spannable text, Object[] tags) {
+        int len = text.length();
+        for (Object tag : tags) {
+            if (len > 0) {
+                text.setSpan(tag, 0, len, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            } else {
+                text.removeSpan(tag);
+            }
+        }
+    }
+
+    /**
+     * Returns a CharSequence that applies a foreground color to the
+     * concatenation of the specified CharSequence objects.
+     */
+    public static CharSequence color(int color, CharSequence... content) {
+        return apply(content, new ForegroundColorSpan(color));
     }
 }
